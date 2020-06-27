2785f05e39e18813c581afdbc560350179d90ccc
zrajani <zainrajani2207@gmail.com>
2020-06-26 23:57:57 -0400

    Updated Code--Lost from Laptop

--- PCM_Regeneration/PCM_Regenerate.m
+++ PCM_Regeneration/PCM_Regenerate.m
@@ -54,4 +54,45 @@ title('Pulse Train Signal');
 xlabel('Discrete Time n \rightarrow');
 ylabel('Amplitude \rightarrow');
 
+%% Amplifier and Equaliser Stage
 
+amplification_factor=2;                % Signal Equalised by
+
+threshold_level=3;                     % Set the threshold level
+
+equaliser_signal=amplification_factor*final_input; % Final Signal Input
+
+figure;                                % Create a new plot
+subplot(3,1,1)                         % Plot first division
+plot(time, equaliser_signal,'g','LineWidth',2)  %Plotting signal
+% Label the plots
+title('Equaliser Amplifer Stage');
+xlabel('Time \rightarrow');
+ylabel('Amplitude \rightarrow');
+
+% Draw threshold line
+line_draw=line([0 100],[threshold_level,threshold_level]); 
+
+% Line Drawn specification (Design for the drawn line)
+
+line_draw.Color='red';
+line_draw.LineStyle='--';
+line_draw.LineWidth=2;
+
+%% Decision Making Block
+
+i=1;
+
+% Loop to decide the signal based on the threshold set 
+while (i<=100)
+    if (equaliser_signal(1,i)>=threshold_level)
+        decision_stage(1,i)=1;
+    else 
+        decision_stage(1,i)=0;
+    end
+    i=i+1;
+end 
+
+disp(decision_stage);            % Display the decision signal
+
+subplot(3,1,3)                   % 3rd Division Plot
